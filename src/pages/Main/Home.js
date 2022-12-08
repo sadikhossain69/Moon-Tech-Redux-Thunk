92e22ea19121cf560f5fa45603df2c8a9f609a28
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-09 00:45:15 +0600

    data calling from backend and some issue fixed

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/products")
+    fetch("https://moon-tech-server-odfw.onrender.com/products")
       .then((res) => res.json())
       .then((data) => setProducts(data.data));
   }, []);
