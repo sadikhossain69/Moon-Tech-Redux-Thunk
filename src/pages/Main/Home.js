81fed0798fdccd2735dee605846faf74d3dcd7b6
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-09 02:09:44 +0600

    Impementing basic filter - part 1 - Adding brands to brands filter array

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import ProductCard from "../../components/ProductCard";
+import { toggleBrand } from "../../redux/actions/filterActions";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const dispatch = useDispatch()
 
   useEffect(() => {
     fetch("https://moon-tech-server-odfw.onrender.com/products")
@@ -20,10 +23,10 @@ const Home = () => {
         >
           In Stock
         </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button onClick={() => dispatch(toggleBrand("amd"))} className={`border px-3 py-2 rounded-full font-semibold`}>
           AMD
         </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button onClick={() => dispatch(toggleBrand("intel"))} className={`border px-3 py-2 rounded-full font-semibold`}>
           Intel
         </button>
       </div>
--- src/redux/reducers/filterReducer.js
+++ src/redux/reducers/filterReducer.js
@@ -1,5 +1,28 @@
-export const initialState = {}
+import { TOGGLE_BRAND, TOGGLE_STOCK } from "../actionTypes/actionTypes"
+
+export const initialState = {
+    filters: {
+        brand: [],
+        stock: false,
+    },
+    keyword: ""
+}
 
 export const filterReducer = (state = initialState, action) => {
-    return state
+    switch (action.type) {
+        case TOGGLE_BRAND:
+            return {
+                ...state,
+                filters: {
+                    ...state.filters,
+                    brand: [...state.filters.brand, action.payload]
+                }
+            }
+        case TOGGLE_STOCK:
+            return {
+
+            }
+        default:
+            return state
+    }
 }
