2b6937b49334622fadcc9e5b9da0364eebef5459
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-09 02:48:47 +0600

    Impementing basic filter - part 2 - Adding Stock toggle and handling filter UI

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
 import { toggleBrand, toggleStock } from "../../redux/actions/filterActions";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const filters = useSelector(state => state.filter.filters)
+  const {brand, stock} = filters
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -20,14 +22,14 @@ const Home = () => {
       <div className='mb-10 flex justify-end gap-5'>
         <button
           onClick={() => dispatch(toggleStock())}
-          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
+          className={`border px-3 py-2 rounded-full font-semibold ${stock ? activeClass : null} `}
         >
           In Stock
         </button>
-        <button onClick={() => dispatch(toggleBrand("amd"))} className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button onClick={() => dispatch(toggleBrand("amd"))} className={`border px-3 py-2 rounded-full font-semibold ${brand.includes('amd') ? activeClass : null}`}>
           AMD
         </button>
-        <button onClick={() => dispatch(toggleBrand("intel"))} className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button onClick={() => dispatch(toggleBrand("intel"))} className={`border px-3 py-2 rounded-full font-semibold ${brand.includes('intel') ? activeClass : null}`}>
           Intel
         </button>
       </div>
